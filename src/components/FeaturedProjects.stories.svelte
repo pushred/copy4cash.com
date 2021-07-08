076fe5cd24765d8bba461c28ab0268387a7a60a1
Eric Casthart <eric@pushred.co>
2021-07-07 23:31:50 -0400

    Add FeaturedProjects component

---
+++ src/components/FeaturedProjects.stories.svelte
@@ -0,0 +1,65 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import FeaturedProjects from './FeaturedProjects.svelte'
+</script>
+
+<Meta
+  title="FeaturedProjects"
+  component={FeaturedProjects}
+  parameters={{ layout: 'centered' }}
+/>
+
+<Story name="Example">
+  <FeaturedProjects
+    data={[
+      [
+        {
+          title: 'Mice Love it to Death',
+          client: 'd-CON',
+        },
+        {
+          title: '#ThatSongWhen',
+          client: 'Spotify',
+        },
+        {
+          title: 'Frenemies',
+          client: 'Coke zero',
+        },
+      ],
+      [
+        {
+          title: 'You Don’t Know zero `Til You’ve Tried It',
+          client: 'Coke zero',
+        },
+      ],
+      [
+        {
+          title: 'Hydrate the Hustle',
+          client: 'vitaminwater',
+        },
+        {
+          title: 'Live Generously',
+          client: 'BJ’s',
+        },
+        {
+          title: '#QuiTips',
+          client: 'Nicorette',
+        },
+      ],
+      [
+        {
+          title: 'BTS Like a Pro',
+          client: 'Staples',
+        },
+        {
+          title: 'More than a Card',
+          client: 'Amex',
+        },
+        {
+          title: 'Print Ain’t Dead',
+          client: 'Lines',
+        },
+      ],
+    ]}
+  />
+</Story>
---
+++ src/components/FeaturedProjects.svelte
@@ -0,0 +1,57 @@
+<script>
+  export let data = []
+</script>
+
+<nav>
+  {#each data as cluster}
+    <ul>
+      {#each cluster as project}
+        <li>
+          <a href="#">
+            <strong>{project.title}</strong>
+            <br />
+            {project.client}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  {/each}
+</nav>
+
+<style>
+  ul {
+    width: 70%;
+    min-width: 75vw;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    text-align: center;
+  }
+
+  ul + ul {
+    margin-top: var(--space-7);
+  }
+
+  li + li {
+    padding-left: var(--space-8);
+    border-left: 2px solid var(--gold);
+    margin-left: var(--space-8);
+  }
+
+  a {
+    color: #fff;
+    font-size: var(--text-1);
+    transition: color 100ms ease-out;
+  }
+
+  a:hover {
+    color: var(--gold);
+  }
+
+  strong {
+    font-size: var(--text-2);
+    font-weight: bold;
+    font-style: italic;
+    text-transform: uppercase;
+  }
+</style>
