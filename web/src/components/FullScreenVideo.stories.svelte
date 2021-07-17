d05a872b26f66c9ffb4f10133e022c62bdcd62ce
Eric Casthart <eric@pushred.co>
2021-07-16 21:19:49 -0400

    Add FullScreenVideo component

---
+++ web/src/components/FullScreenVideo.stories.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Meta, Story } from '@storybook/addon-svelte-csf'
+  import FullScreenVideo from './FullScreenVideo.svelte'
+</script>
+
+<Meta title="FullScreenVideo" component={FullScreenVideo} />
+
+<Story name="Example">
+  <FullScreenVideo vimeoId="334283806" />
+</Story>
---
+++ web/src/components/FullScreenVideo.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let backdrop = true
+  export let vimeoId = undefined
+</script>
+
+<div class:backdrop>
+  {#if vimeoId}
+    <iframe
+      title="Video"
+      src="https://player.vimeo.com/video/{vimeoId}?background=1&autoplay=1&loop=1&byline=0&title=0"
+    />
+  {/if}
+</div>
+
+<style>
+  div {
+    z-index: -1;
+    pointer-events: none;
+    overflow: hidden;
+    background: var(--black);
+  }
+
+  div,
+  div:before {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  .backdrop:before {
+    opacity: 0.85;
+    content: '';
+    background-color: #0e0024;
+    z-index: 1;
+  }
+
+  /* from https://codepen.io/abennington/pen/ZONqqv */
+
+  iframe {
+    width: 100vw;
+    height: 56.25vw; /* given a 16:9 aspect ratio, 9/16*100 = 56.25 */
+    min-height: 100vh;
+    min-width: 177.77vh; /* given a 16:9 aspect ratio, 16/9*100 = 177.77 */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
