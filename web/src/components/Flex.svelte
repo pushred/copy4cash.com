e626950f743dc0ecb8556beaa6001777a0d14a29
Eric Casthart <eric@pushred.co>
2021-07-18 18:25:04 -0400

    Add Flex component
    For ad hoc layouts

---
+++ web/src/components/Flex.svelte
@@ -0,0 +1,37 @@
+<script>
+  export let alignItems
+  export let position
+  export let justifyContent
+  export let inset
+</script>
+
+<div
+  class:absolute={position === 'absolute'}
+  class:inset-0={inset === '0'}
+  class:items-center={alignItems === 'center'}
+  class:justify-center={justifyContent === 'center'}
+>
+  <slot />
+</div>
+
+<style>
+  div {
+    display: flex;
+  }
+
+  .absolute {
+    position: absolute;
+  }
+
+  .inset-0 {
+    inset: 0;
+  }
+
+  .items-center {
+    align-items: center;
+  }
+
+  .justify-center {
+    justify-content: center;
+  }
+</style>
