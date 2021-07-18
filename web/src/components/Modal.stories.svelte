964b9e4625bc86278989e0e55a66bfe81c6cdd99
Eric Casthart <eric@pushred.co>
2021-07-18 18:25:45 -0400

    Add Modal component
    * Render content in an ovelray
    * Dismiss with Close button or `Escape` key

---
+++ web/src/components/Modal.stories.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import ImageViewer from './ImageViewer.svelte'
+  import Flex from './Flex.svelte'
+  import Modal from './Modal.svelte'
+  import Video from './Video.svelte'
+</script>
+
+<Meta
+  title="Modal"
+  component={Modal}
+  argTypes={{
+    isOpen: {
+      control: { type: 'boolean' },
+    },
+  }}
+/>
+
+<Template let:args>
+  <Flex
+    position="absolute"
+    inset="0"
+    alignItems="center"
+    justifyContent="center"
+  >
+    <Video vimeoId="336888265" originalWidth="426" originalHeight="240" />
+  </Flex>
+  <Modal {...args}>
+    <ImageViewer
+      data={[
+        {
+          description: 'Cat',
+          url: 'https://placekitten.com/500/300',
+        },
+        {
+          description: 'Cat',
+          url: 'https://placekitten.com/600/400',
+        },
+        {
+          description: 'Cat',
+          url: 'https://placekitten.com/900/500',
+        },
+      ]}
+    />
+  </Modal>
+</Template>
+
+<Story name="Example" args={{ isOpen: true }} />
---
+++ web/src/components/Modal.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { fade } from 'svelte/transition'
+
+  import Button from './Button.svelte'
+
+  export let isOpen = false
+
+  function handleClose(event) {
+    isOpen = false
+    console.log('yo')
+  }
+
+  function handleKeydown(event) {
+    if (event.key === 'Escape') {
+      isOpen = false
+    }
+  }
+</script>
+
+<svelte:window on:keydown={handleKeydown} />
+
+{#if isOpen}
+  <div transition:fade={{ duration: 200 }}>
+    <div class="content">
+      <slot />
+      <Button
+        class="button"
+        icon="close"
+        label="Close"
+        on:click={handleClose}
+      />
+    </div>
+    <div class="backdrop" aria-hidden />
+  </div>
+{/if}
+
+<style>
+  .backdrop {
+    position: fixed;
+    inset: 0;
+    background-color: var(--black);
+    opacity: 0.5;
+    z-index: var(--z-0);
+  }
+
+  .content {
+    position: fixed;
+    inset: 0;
+    z-index: var(--z-10);
+  }
+
+  :global(.button) {
+    position: absolute;
+    top: var(--space-8);
+    right: var(--space-8);
+  }
+</style>
