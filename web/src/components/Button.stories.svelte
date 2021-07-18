f50728ae5c8812e772989cbe47a3f517a5e1078f
Eric Casthart <eric@pushred.co>
2021-07-18 17:49:34 -0400

    Add Button component
    * next, back, close icons
    * Style interactive states
    * Allow external styles by class

---
+++ web/src/components/Button.stories.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Button from './Button.svelte'
+</script>
+
+<Meta
+  title="Button"
+  component={Button}
+  argTypes={{
+    icon: {
+      options: ['next', 'back', 'close'],
+      control: { type: 'select' },
+    },
+  }}
+/>
+
+<Template let:args>
+  <Button {...args} />
+</Template>
+
+<Story name="Icon" args={{ icon: 'next', label: 'Next' }} />
