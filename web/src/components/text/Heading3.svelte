f71afbba438dbbc25089c380008cdddb2a4a4b45
Eric Casthart <eric@pushred.co>
2021-08-13 09:15:34 -0400

    Wrap + stylize Heading3 parenthesized text

--- web/src/components/text/Heading3.svelte
+++ web/src/components/text/Heading3.svelte
@@ -1,4 +1,33 @@
-<h3><slot /></h3>
+<script>
+  import { onMount } from 'svelte'
+  import unorphan from 'unorphan'
+
+  let el
+  let heading
+  let subheading
+  let isProcessed
+
+  onMount(() => {
+    const text = el.textContent
+    const splitText = /(.*)(\(.*\))/.exec(text)
+
+    unorphan('h3')
+
+    heading = splitText ? splitText[1] : text
+    heading = heading.replace('?', '?<br />')
+    subheading = splitText ? splitText[2] : null
+    isProcessed = true
+  })
+</script>
+
+<h3 bind:this={el}>
+  {#if isProcessed}
+    {@html heading}
+    {#if subheading}<span class="subheading">{subheading}</span>{/if}
+  {:else}
+    <slot />
+  {/if}
+</h3>
 
 <style>
   h3 {
@@ -8,5 +37,10 @@
     font-weight: bold;
     line-height: 1.5;
     color: var(--purple);
+  }
+
+  .subheading {
+    display: block;
+    font-size: var(--text-2);
   }
 </style>
