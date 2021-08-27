ec9539e354c11cf3ee125b24a7550a897428880e
Eric Casthart <eric@pushred.co>
2021-08-27 19:29:26 -0400

    Pagination dispatcher

--- web/src/components/Pagination.svelte
+++ web/src/components/Pagination.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { createEventDispatcher } from 'svelte'
+
   import Button from './Button.svelte'
   import Flex from './Flex.svelte'
   import PaginationDot from './icons/small/PaginationDot.svelte'
@@ -6,34 +8,31 @@
   export let currentIndex = undefined
   export let totalPages = 10
 
-  function handleBack() {
-    currentIndex = currentIndex - 1
-  }
+  const dispatch = createEventDispatcher()
 
-  function handleClick(index) {
-    currentIndex = index
-  }
-
-  function handleNext() {
-    currentIndex = currentIndex + 1
+  function handleChange(selection) {
+    if (selection === 'back') currentIndex = currentIndex - 1
+    if (selection === 'next') currentIndex = currentIndex + 1
+    if (typeof selection === 'number') currentIndex = selection
+    dispatch('change', currentIndex)
   }
 </script>
 
 <Flex justifyContent="space-between">
   <Button
     disabled={currentIndex === 0}
-    on:click={handleBack}
+    on:click={handleChange.bind(null, 'back')}
     icon="left-arrows"
   />
   {#each [...Array(totalPages).keys()] as page, index}
     <PaginationDot
       active={index === currentIndex}
-      on:click={handleClick.bind(null, index)}
+      on:click={handleChange.bind(null, index)}
     />
   {/each}
   <Button
     disabled={currentIndex === totalPages - 1}
-    on:click={handleNext}
+    on:click={handleChange.bind(null, 'next')}
     icon="right-arrows"
   />
 </Flex>
