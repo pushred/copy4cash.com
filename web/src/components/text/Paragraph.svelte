591015e0a53c73f1fcc287959ebf8f6c91a89ada
Eric Casthart <eric@pushred.co>
2021-09-04 12:55:22 -0400

    Add paragraph spacing

--- web/src/components/text/Paragraph.svelte
+++ web/src/components/text/Paragraph.svelte
@@ -2,8 +2,13 @@
 
 <style>
   p {
+    margin-bottom: 1.5em;
     font-size: var(--text-2);
     line-height: 1.5;
     color: var(--white);
+  }
+
+  p:last-child {
+    margin-bottom: 0;
   }
 </style>
