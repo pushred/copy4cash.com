78ea000e95baab648f7d2147983793eba445d511
Eric Casthart <eric@pushred.co>
2021-09-10 16:17:28 -0400

    Use Sanity CDN for non-local/preview envs

--- web/src/sanity.js
+++ web/src/sanity.js
@@ -1,12 +1,14 @@
 import sanityClient from '@sanity/client'
 
+const isLocal = window?.location?.hostname === 'localhost'
+const isPreview = window?.location?.hostname.splits('.').shift() === 'preview'
+
 const client = sanityClient({
   projectId: '3wd8aqoe',
   dataset: 'production',
   apiVersion: '2021-04-04',
-  useCdn: false,
-  withCredentials: true,
-  // unauthenticated for now
+  useCdn: !isLocal && !isPreview,
+  withCredentials: isLocal || isPreview,
 })
 
 export default client
