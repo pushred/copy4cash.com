4e5003842c93eaac8b99bf02d171a3118826eaab
Eric Casthart <eric@pushred.co>
2021-09-10 15:14:27 -0400

    Coins separator

--- web/src/components/Separator.svelte
+++ web/src/components/Separator.svelte
@@ -1,15 +1,92 @@
 <svg
-  width="6"
-  height="34"
-  viewBox="0 0 6 34"
+  width="12"
+  height="40"
+  viewBox="0 0 12 40"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
 >
-  <circle cx="3" cy="3" r="2" fill="var(--neon-green)" />
-  <circle cx="3" cy="17" r="2" fill="var(--neon-green)" />
-  <circle cx="3" cy="31" r="2" fill="var(--neon-green)" />
-
-  <circle cx="3" cy="3" r="0.75" fill="var(--white)" />
-  <circle cx="3" cy="17" r="0.75" fill="var(--white)" />
-  <circle cx="3" cy="31" r="0.75" fill="var(--white)" />
+  <path
+    d="M1 3.93056V6.23611C1 7.38889 2.92308 8.54167 6 8.54167C9.07692 8.54167 11 7.38889 11 6.23611V3.93056"
+    stroke="#FFB32C"
+    stroke-width="1.4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6 6.23611C8.76142 6.23611 11 5.20388 11 3.93056C11 2.65723 8.76142 1.625 6 1.625C3.23858 1.625 1 2.65723 1 3.93056C1 5.20388 3.23858 6.23611 6 6.23611Z"
+    stroke="#FFB32C"
+    stroke-width="1.4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M1 3.93056V6.23611C1 7.38889 2.92308 8.54167 6 8.54167C9.07692 8.54167 11 7.38889 11 6.23611V3.93056"
+    stroke="white"
+    stroke-width="0.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6 6.23611C8.76142 6.23611 11 5.20388 11 3.93056C11 2.65723 8.76142 1.625 6 1.625C3.23858 1.625 1 2.65723 1 3.93056C1 5.20388 3.23858 6.23611 6 6.23611Z"
+    stroke="white"
+    stroke-width="0.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M1 18.8472V21.1528C1 22.3056 2.92308 23.4583 6 23.4583C9.07692 23.4583 11 22.3056 11 21.1528V18.8472"
+    stroke="#FFB32C"
+    stroke-width="1.4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6 21.1528C8.76142 21.1528 11 20.1205 11 18.8472C11 17.5739 8.76142 16.5417 6 16.5417C3.23858 16.5417 1 17.5739 1 18.8472C1 20.1205 3.23858 21.1528 6 21.1528Z"
+    stroke="#FFB32C"
+    stroke-width="1.4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M1 18.8472V21.1528C1 22.3056 2.92308 23.4583 6 23.4583C9.07692 23.4583 11 22.3056 11 21.1528V18.8472"
+    stroke="white"
+    stroke-width="0.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6 21.1528C8.76142 21.1528 11 20.1205 11 18.8472C11 17.5739 8.76142 16.5417 6 16.5417C3.23858 16.5417 1 17.5739 1 18.8472C1 20.1205 3.23858 21.1528 6 21.1528Z"
+    stroke="white"
+    stroke-width="0.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M1 33.7639V36.0694C1 37.2222 2.92308 38.375 6 38.375C9.07692 38.375 11 37.2222 11 36.0694V33.7639"
+    stroke="#FFB32C"
+    stroke-width="1.4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6 36.0694C8.76142 36.0694 11 35.0372 11 33.7639C11 32.4906 8.76142 31.4583 6 31.4583C3.23858 31.4583 1 32.4906 1 33.7639C1 35.0372 3.23858 36.0694 6 36.0694Z"
+    stroke="#FFB32C"
+    stroke-width="1.4"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M1 33.7639V36.0694C1 37.2222 2.92308 38.375 6 38.375C9.07692 38.375 11 37.2222 11 36.0694V33.7639"
+    stroke="white"
+    stroke-width="0.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6 36.0694C8.76142 36.0694 11 35.0372 11 33.7639C11 32.4906 8.76142 31.4583 6 31.4583C3.23858 31.4583 1 32.4906 1 33.7639C1 35.0372 3.23858 36.0694 6 36.0694Z"
+    stroke="white"
+    stroke-width="0.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
 </svg>
