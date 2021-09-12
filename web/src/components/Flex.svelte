c8d7a20d7bb86ecadb68000653ba65072c7398ad
Eric Casthart <eric@pushred.co>
2021-09-12 11:34:44 -0400

    Add Flex responsive props

--- web/src/components/Flex.svelte
+++ web/src/components/Flex.svelte
@@ -1,26 +1,53 @@
 <script>
+  import { getResponsivePropValues, onBreakpointChange } from '../theme.js'
+
   export let alignItems = undefined
+  export let flexDirection = undefined
   export let gap = undefined
-  export let position = undefined
-  export let justifyContent = undefined
   export let inset = undefined
+  export let justifyContent = undefined
+  export let position = undefined
+
+  let props = {
+    alignItems,
+    flexDirection,
+    gap,
+    position,
+    justifyContent,
+    inset,
+  }
+
+  $: {
+    onBreakpointChange((breakpoint) => {
+      props = getResponsivePropValues(breakpoint, {
+        alignItems,
+        flexDirection,
+        gap,
+        position,
+        justifyContent,
+        inset,
+      })
+      console.log(breakpoint, props)
+    })
+  }
 </script>
 
 <div
-  class:absolute={position === 'absolute'}
-  class:inset-0={inset === '0'}
-  class:items-center={alignItems === 'center'}
-  class:justify-center={justifyContent === 'center'}
-  class:justify-space-between={justifyContent === 'space-between'}
-  class:gap-2={gap === '2'}
-  class:gap-3={gap === '3'}
-  class:gap-4={gap === '4'}
-  class:gap-5={gap === '5'}
-  class:gap-6={gap === '6'}
-  class:gap-7={gap === '7'}
-  class:gap-8={gap === '8'}
-  class:gap-9={gap === '9'}
-  class:gap-10={gap === '10'}
+  class:absolute={props.position === 'absolute'}
+  class:flex-direction-column={props.flexDirection === 'column'}
+  class:inset-0={props.inset === '0'}
+  class:items-center={props.alignItems === 'center'}
+  class:justify-center={props.justifyContent === 'center'}
+  class:justify-space-between={props.justifyContent === 'space-between'}
+  class:gap-2={props.gap === '2'}
+  class:gap-3={props.gap === '3'}
+  class:gap-4={props.gap === '4'}
+  class:gap-5={props.gap === '5'}
+  class:gap-6={props.gap === '6'}
+  class:gap-7={props.gap === '7'}
+  class:gap-8={props.gap === '8'}
+  class:gap-9={props.gap === '9'}
+  class:gap-10={props.gap === '10'}
 >
   <slot />
 </div>
@@ -37,6 +64,10 @@
 
   .absolute {
     position: absolute;
+  }
+
+  .flex-direction-column {
+    flex-direction: column;
   }
 
   .inset-0 {
