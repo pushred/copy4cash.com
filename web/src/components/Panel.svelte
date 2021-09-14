817ab1d3dd60aa846e699daefdc3d32b3fbcfac9
Eric Casthart <eric@pushred.co>
2021-09-13 20:14:17 -0400

    Panel component
    Extract from Tweet for reuse

---
+++ web/src/components/Panel.svelte
@@ -0,0 +1,13 @@
+<div>
+  <slot />
+</div>
+
+<style>
+  div {
+    margin: 3px 3px var(--space-bottom) 3px;
+    padding: var(--space-5);
+    border: 1px solid var(--purple);
+    border-radius: var(--space-5);
+    box-shadow: 0 0 2px var(--purple), 0 0 3px var(--purple);
+  }
+</style>
--- web/src/components/Tweet.svelte
+++ web/src/components/Tweet.svelte
@@ -1,38 +1,33 @@
 <script>
   export let html
+
+  import Panel from './Panel.svelte'
 </script>
 
-<blockquote class="tweet">
-  {@html html}
-</blockquote>
+<Panel>
+  <blockquote class="html">
+    {@html html}
+  </blockquote>
+</Panel>
 
 <style>
-  .tweet {
-    margin: 3px 3px 0 3px;
-    padding: var(--space-5);
-    border: 1px solid var(--purple);
-    border-radius: var(--space-5);
-    box-shadow: 0 0 2px var(--purple), 0 0 3px var(--purple);
-    font-family: var(--text-font);
-    font-size: var(--text-2);
-    line-height: 1.5;
+  .html {
     color: var(--gold);
-    text-align: center;
   }
 
-  .tweet :global(p) {
+  .html :global(p) {
     color: var(--white);
   }
 
-  .tweet :global(p:last-of-type) {
+  .html :global(p:last-of-type) {
     margin-bottom: var(--space-3);
   }
 
-  .tweet :global(p a) {
+  .html :global(p a) {
     color: var(--light-purple);
   }
 
-  .tweet > :global(a) {
+  .html > :global(a) {
     display: none;
   }
 </style>
