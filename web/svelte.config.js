58417a2dccef1d0ef913a062f051555188885d1b
Eric Casthart <eric@pushred.co>
2021-10-02 13:55:11 -0400

    Try adapter-static fallback
    Overcomplicating and not reading the manual seems to be the issue!

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -3,7 +3,9 @@ import staticAdapter from '@sveltejs/adapter-static'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: staticAdapter(),
+    adapter: staticAdapter({
+      fallback: '404.html',
+    }),
   },
 }
 
